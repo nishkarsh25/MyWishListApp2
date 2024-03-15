aaba755bc9a7e83b1211ae86b1a12fd4a4fd6816
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-03-15 13:45:16 +0530

    Adding dependencies to build gradle(app level)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,14 +1,15 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
 }
 
 android {
-    namespace = "eu.tutorials.mywishlistapp2"
+    namespace = "eu.tutorials.mywishlistapp"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "eu.tutorials.mywishlistapp2"
+        applicationId = "eu.tutorials.mywishlistapp"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
@@ -51,9 +52,25 @@ android {
 
 dependencies {
 
+    val nav_version = "2.7.5"
+    val compose_version = "1.6.0-alpha06"
+    val room = "2.6.0"
+
+    // Room
+    implementation("androidx.room:room-runtime:$room")
+    implementation("androidx.room:room-ktx:$room")
+    kapt("androidx.room:room-compiler:$room")
+
+    implementation("androidx.navigation:navigation-compose:$nav_version")
+    implementation("androidx.compose.ui:ui:$compose_version")
+    implementation("androidx.compose.material:material:$compose_version")
+    implementation("androidx.compose.ui:ui-tooling-preview:$compose_version")
+
+
+
     implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    implementation("androidx.activity:activity-compose:1.8.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.activity:activity-compose:1.8.0")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
