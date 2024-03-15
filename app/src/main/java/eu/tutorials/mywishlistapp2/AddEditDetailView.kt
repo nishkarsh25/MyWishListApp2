37476441ee413fe349b042320be5c8924a892691
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-03-15 14:55:15 +0530

    rectified an error in Update An Existing Item commit

--- app/src/main/java/eu/tutorials/mywishlistapp2/AddEditDetailView.kt
+++ app/src/main/java/eu/tutorials/mywishlistapp2/AddEditDetailView.kt
@@ -90,7 +90,7 @@ fun AddEditDetailView(
                 if(viewModel.wishTitleState.isNotEmpty() &&
                     viewModel.wishDescriptionState.isNotEmpty()){
                     if(id != 0L){
-                        viewModel.updateWishh(
+                        viewModel.updateWish(
                             Wish(
                                 id = id,
                                 title = viewModel.wishTitleState.trim(),
